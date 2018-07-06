9643af0925c9e14edb411a4c3613c11802a32749
ASound18 <g.t.tongchuan@gmail.com>
2018-07-06 17:58:19 +0800

    Add files via upload

---
+++ server.R
@@ -0,0 +1,146 @@
+options(shiny.host="10.64.20.23")
+options(shiny.port=7401)
+
+library(shiny)
+library(stringr)
+
+# returns string w/o leading or trailing whitespace
+trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+
+# Globally define a place where all users can share some reactive data.
+vars <- reactiveValues(chat=NULL, users=NULL)
+
+# Restore the chat log from the last session.
+if (file.exists("chat.Rds")){
+  vars$chat <- readRDS("chat.Rds")
+} else {
+  vars$chat <- "Welcome to SilentChat!"
+}
+
+#' Get the prefix for the line to be added to the chat window. Usually a newline
+#' character unless it's the first line.
+linePrefix <- function(){
+  if (is.null(isolate(vars$chat))){
+    return("")
+  }
+  return("<br />")
+}
+
+shinyServer(function(input, output, session) {
+  # Create a spot for reactive variables specific to this particular session
+  sessionVars <- reactiveValues(username = "")
+  
+  # Track whether or not this session has been initialized. We'll use this to
+  # assign a username to unininitialized sessions.
+  init <- FALSE
+  
+  # When a session is ended, remove the user and note that they left the room. 
+  session$onSessionEnded(function() {
+    isolate({
+      vars$users <- vars$users[vars$users != sessionVars$username]
+      vars$chat <- c(vars$chat, paste0(linePrefix(),
+                                       tags$span(class="user-exit",
+                                                 sessionVars$username,
+                                                 "left the room.")))
+    })
+  })
+  
+  # Observer to handle changes to the username
+  observe({
+    # We want a reactive dependency on this variable, so we'll just list it here.
+    input$user
+    
+    if (!init){
+      # Seed initial username
+      sessionVars$username <- paste0("User", round(runif(1, 10000, 99999)))
+      isolate({
+        vars$chat <<- c(vars$chat, paste0(linePrefix(),
+                                          tags$span(class="user-enter",
+                                                    sessionVars$username,
+                                                    "entered the room.")))
+      })
+      init <<- TRUE
+    } else{
+      # A previous username was already given
+      isolate({
+        if (input$user == sessionVars$username || input$user == ""){
+          # No change. Just return.
+          return()
+        }
+        
+        # Updating username      
+        # First, remove the old one
+        vars$users <- vars$users[vars$users != sessionVars$username]
+        
+        # Note the change in the chat log
+        vars$chat <<- c(vars$chat, paste0(linePrefix(),
+                                          tags$span(class="user-change",
+                                                    paste0("\"", sessionVars$username, "\""),
+                                                    " -> ",
+                                                    paste0("\"", input$user, "\""))))
+        
+        # Now update with the new one
+        sessionVars$username <- input$user
+      })
+    }
+    # Add this user to the global list of users
+    isolate(vars$users <- c(vars$users, sessionVars$username))
+  })
+  
+  # Keep the username updated with whatever sanitized/assigned username we have
+  observe({
+    updateTextInput(session, "user", 
+                    value=sessionVars$username)    
+  })
+  
+  # Keep the list of connected users updated
+  output$userList <- renderUI({
+    tagList(tags$ul( lapply(vars$users, function(user){
+      return(tags$li(user))
+    })))
+  })
+  
+  # Listen for input$send changes (i.e. when the button is clicked)
+  observe({
+    if(input$send < 1){
+      # The code must be initializing, b/c the button hasn't been clicked yet.
+      return()
+    }
+    isolate({
+      # Add the current entry to the chat log.
+      if (trim(input$entry) != "") {
+        vars$chat <<- c(vars$chat, 
+                        paste0(linePrefix(),
+                               tags$span(class="username",
+                                         tags$abbr(title=Sys.time(), sessionVars$username)
+                               ),
+                               ": ",
+                               tagList(input$entry)))
+      }
+    })
+    # Clear out the text entry field.
+    updateTextInput(session, "entry", value="") 
+  })
+  
+  # Listen for the input$clearChat (i.e. when the button is clicked)
+  observeEvent(input$clearChat, {
+    vars$chat <<- "Welcome to SilentChat!"
+  })
+  
+  # Dynamically create the UI for the chat window.
+  output$chat <- renderUI({
+    if (length(vars$chat) > 500){
+      # Too long, use only the most recent 500 lines
+      vars$chat <- vars$chat[(length(vars$chat)-500):(length(vars$chat))]
+    }
+    # Save the chat object so we can restore it later if needed.
+    if (input$mode_selection == "Log Mode"){
+      saveRDS(vars$chat, "chat.Rds")
+    } else if (input$mode_selection == "Silent Mode"){
+      saveRDS(vars$chat, gsub(":", "_", paste0("SilentLogs/chat_", Sys.time(), ".Rds")))
+    }
+    
+    # Pass the chat log through as HTML
+    HTML(vars$chat)
+  })
+})
